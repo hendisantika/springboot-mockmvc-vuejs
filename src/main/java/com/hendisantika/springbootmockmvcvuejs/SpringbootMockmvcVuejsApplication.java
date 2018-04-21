ce1d9d6b799b10b7cff329db0fcbb2bb58ad3522
Hendi Santika <hendisantika@gmail.com>
2018-04-21 18:05:07 +0700

    #13 - Modify Main App

--- src/main/java/com/hendisantika/springbootmockmvcvuejs/SpringbootMockmvcVuejsApplication.java
+++ src/main/java/com/hendisantika/springbootmockmvcvuejs/SpringbootMockmvcVuejsApplication.java
@@ -1,12 +1,23 @@
 package com.hendisantika.springbootmockmvcvuejs;
 
+import com.hendisantika.springbootmockmvcvuejs.model.StockData;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
-public class SpringbootMockmvcVuejsApplication {
+public class SpringbootMockmvcVuejsApplication implements CommandLineRunner {
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMockmvcVuejsApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) {
+        logger.info("Initialize data...");
+        StockData.INSTANCE.initData();
     }
 }
