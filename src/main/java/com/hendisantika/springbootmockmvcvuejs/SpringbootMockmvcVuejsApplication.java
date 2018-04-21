cd7d83134b1d837f1d93a61241b40ca721037a9f
Hendi Santika <hendisantika@gmail.com>
2018-04-21 17:40:49 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmockmvcvuejs/SpringbootMockmvcVuejsApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootmockmvcvuejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMockmvcVuejsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMockmvcVuejsApplication.class, args);
+    }
+}
